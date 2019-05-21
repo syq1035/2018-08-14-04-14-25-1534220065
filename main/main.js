764eb2bc0a775f57d8ac480363f719552595ca94
fireflyq <1035394794@qq.com>
2019-05-21 20:50:58 +0800

    success

--- main/main.js
+++ main/main.js
@@ -1,4 +1,21 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(s_num) {
+    let lcdData = [
+        ["._.","...","._.","._.","...","._.","._.","._.","._.","._."],
+        ["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"],
+        ["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"]
+    ]
+
+    let top = "";
+    let middle = "";
+    let bottom = "";
+
+    for(let i = 0; i<s_num.length; i++){
+        top += lcdData[0][parseInt(s_num[i])] + " ";
+        middle += lcdData[1][parseInt(s_num[i])] + " ";
+        bottom += lcdData[2][parseInt(s_num[i])] + " ";
+    }
+
+    let result  = top.trim() + "\n" + middle.trim()+ "\n" + bottom.trim() + "\n";
+
+    return result;
 };
